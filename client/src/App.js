d43248bfba245419c184c7c3cb5055b1aa323714
JeevanMKJ <jeevanjones.jmkj@gmail.com>
2023-05-01 20:36:47 -0500

    added signup/login code

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import SignUpLoginInPage from "./pages/SignUpLogInPage";
 
 const client = new ApolloClient({
   uri: "/graphql",
@@ -8,7 +9,11 @@ const client = new ApolloClient({
 });
 
 function App() {
-  return <ApolloProvider client={client}></ApolloProvider>;
+  return (
+    <ApolloProvider client={client}>
+      <SignUpLoginInPage />
+    </ApolloProvider>
+  );
 }
 
 export default App;
