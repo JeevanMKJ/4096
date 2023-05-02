677d49216f5480271bddde901b1b04d25c178094
brennaveir <brennaveir@hotmail.com>
2023-05-01 20:33:26 -0400

    added user and score schema models

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,13 @@
 const { gql } = require("apollo-server-express");
 
-const typeDefs = gql``;
+const typeDefs = gql`
+type User {
+    _id: ID!
+    username: String!
+    email: String!
+    password: String!
+    scores: [Scores]
+  }
+`;
 
 module.exports = typeDefs;
