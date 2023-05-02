155f147eb56ebfcdf525d724e34f12d6b10b95f8
Priyanka Agrawal <priyankaagrawal@Priyankas-MacBook-Pro.local>
2023-05-01 21:23:31 -0400

    makeshift grid

---
+++ client/src/components/grid.js
@@ -0,0 +1,78 @@
+import React from "react";
+
+function Grid() {
+    return (
+        <>
+            <div class="container md">
+                <div className="grid grid-cols-8 grid-rows-8 gap-1 border">
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                    <div className="border">01</div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Grid;
