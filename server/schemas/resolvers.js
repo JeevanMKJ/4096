99bccd6f0ee89db0210794c2f92e7dfb816a654d
brennaveir <brennaveir@hotmail.com>
2023-05-04 19:25:10 -0400

    added queries on client side

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -16,6 +16,7 @@ const resolvers = {
     scores: async (parent, { username }) => {
       const params = username ? { username } : {};
       return Scores.find(params).sort({ createdAt: -1 });
+    },
     me: async (parent, args, context) => {
       if (context.user) {
         return User.findOne({ _id: context.user._id });
