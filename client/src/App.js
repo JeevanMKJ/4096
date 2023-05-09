ec884fc432b06ba8a46e82473e598e39494927e5
JeevanMKJ <jeevanjones.jmkj@gmail.com>
2023-05-09 09:27:45 -0500

    rewrote game logic - works now

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,32 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink, } from "@apollo/client";
-import { setContext } from '@apollo/client/link/context';
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
 import Navigation from "./components/Navigation.js";
+import Game from "./components/Game.js";
 import Footer from "./components/FooterComponent.js";
-import Game from "./components/GameComponent.js";
 import HighScores from "./components/HighScoresComponent.js";
 import HowToPlay from "./components/HowToPlayComponent.js";
 import Profile from "./components/ProfileComponent.js";
 import Login from "./pages/Login.js";
 import Signup from "./pages/Signup.js";
 
-
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: "/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
+  const token = localStorage.getItem("id_token");
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : '',
+      authorization: token ? `Bearer ${token}` : "",
     },
   };
 });
@@ -34,42 +38,24 @@ const client = new ApolloClient({
 
 function App() {
   return (
-    <ApolloProvider client={client} className='bg-sweater'>
+    <ApolloProvider client={client} className="bg-sweater">
       <Router>
         <div className="flex-column justify-flex-start min-100-vh">
           <Navigation />
           <div className="container">
             <Routes>
-              <Route 
-                path="/" 
-                element={<Game />}
-              />
-               <Route 
-                path="/highscores" 
-                element={<HighScores />}
-              />
-                <Route 
-                path="/howtoplay" 
-                element={<HowToPlay />}
-              />
-              <Route 
-                path="/me" 
-                element={<Profile />}
-              />
-               <Route 
-                path="/login" 
-                element={<Login />}
-              />
-              <Route 
-                path="/signup" 
-                element={<Signup />}
-              />
-              </Routes>
+              <Route path="/" element={<Game />} />
+              <Route path="/highscores" element={<HighScores />} />
+              <Route path="/howtoplay" element={<HowToPlay />} />
+              <Route path="/me" element={<Profile />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+            </Routes>
           </div>
           <Footer />
         </div>
       </Router>
-    </ApolloProvider >
+    </ApolloProvider>
   );
 }
 
