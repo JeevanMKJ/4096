6bf48b014ed3fbdabe957595edf3de99271df732
tylerjwait <118226586+tylerjwait@users.noreply.github.com>
2023-05-10 19:09:05 -0400

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/4096_DB"
-);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/4096_DB');
 
 module.exports = mongoose.connection;
