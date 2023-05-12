e2a5fe4a7bc779ff9ec2b46064cd17bbfd41ee78
Priyanka Agrawal <priyankaagrawal@Priyankas-MacBook-Pro.local>
2023-05-11 21:29:06 -0400

    fixed the footer issues

--- client/src/components/FooterComponent.js
+++ client/src/components/FooterComponent.js
@@ -64,28 +64,30 @@ const navigation = [
 
 export default function Example() {
   return (
-    <div className="fixed bottom-0 right-0 left-0 width-full">
-    <footer className="bg-white font-serif">
-      <div className="mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
-        <div className="flex justify-center space-x-6 md:order-2">
-          {navigation.map((item) => (
-            <a
-              key={item.name}
-              href={item.href}
-              className="text-gray-400 hover:text-gray-500"
-            >
-              <span className="sr-only">{item.name}</span>
-              <item.icon className="h-6 w-6" aria-hidden="true" />
-            </a>
-          ))}
-        </div>
-        <div className="mt-8 md:order-1 md:mt-0">
-          <p className="text-center text-xs leading-5 text-gray-500">
-            &copy; 2020 Your Company, Inc. All rights reserved.
-          </p>
-        </div>
+    <div className="h-screen">
+      <div className="fixed bottom-0 right-0 left-0 width-full">
+        <footer className="bg-white font-serif">
+          <div className="mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
+            <div className="flex justify-center space-x-6 md:order-2">
+              {navigation.map((item) => (
+                <a
+                  key={item.name}
+                  href={item.href}
+                  className="text-gray-400 hover:text-gray-500"
+                >
+                  <span className="sr-only">{item.name}</span>
+                  <item.icon className="h-6 w-6" aria-hidden="true" />
+                </a>
+              ))}
+            </div>
+            <div className="mt-8 md:order-1 md:mt-0">
+              <p className="text-center text-xs leading-5 text-gray-500">
+                &copy; 2020 Your Company, Inc. All rights reserved.
+              </p>
+            </div>
+          </div>
+        </footer>
       </div>
-    </footer>
     </div>
   );
 }
